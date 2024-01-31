4dc3b02fffb9fbe2c6a67525d769796a9f2fdd4f
nishita-esrat <nishitaesrat120@gmail.com>
2024-01-31 21:59:46 +0600

    get toy route and create toy route is done

--- server/index.js
+++ server/index.js
@@ -21,19 +21,65 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // database collection
+    const database = client.db("toyDB");
+    const collection_of_toy = database.collection("toys");
+    // for checking route
+    app.get("/health", (req, res) => {
+      res.send("hello");
+    });
+    // creating new toy
+    app.post("/addToy", async (req, res) => {
+      const toy = req.body;
+      const result = await collection_of_toy.insertOne(toy);
+      console.log(result);
+      res.send(result);
+    });
+    // get route based on the toyName,subCategory,sellerEmail,page,limit,orderAscOrDes
+    app.get("/toys", async (req, res) => {
+      // receive info
+      const { toyName, subCategory, sellerEmail, page, limit, orderAscOrDes } =
+        req.query;
+        console.log(req.query)
+      // for odering ascending or decending based on price
+      const oderBasedOnPrice = orderAscOrDes == "ascending" ? 1 : -1;
+      console.log(oderBasedOnPrice)
+      let option = {};
+      // for limit and skip toy item
+      const limitToy = parseInt(limit) || (await collection_of_toy.countDocuments({}));
+      const skipToy = parseInt(limit) * ((parseInt(page) || 1) - 1);
+      // for checking which type of toy data , we want
+      let filter = {};
+      if (toyName) {
+        filter = { toy_name: toyName };
+      } else if (subCategory) {
+        filter = { sub_category: subCategory };
+      } else if (sellerEmail) {
+        filter = { seller_email: sellerEmail };
+      } else {
+        filter = {};
+      }
+      if (orderAscOrDes) {
+        option = {
+          sort: {
+            price: oderBasedOnPrice,
+          },
+        };
+      }
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+      const result = await collection_of_toy
+        .find(filter, option)
+        .skip(skipToy)
+        .limit(limitToy)
+        .toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-
-
+run();
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
