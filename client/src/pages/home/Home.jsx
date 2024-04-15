e3910000c04329e8308513b1d382c3f9223c6cee
nishita-esrat <nishitaesrat120@gmail.com>
2024-04-15 21:13:53 +0600

    sub category section is creating

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Hero from './Hero'
+import SubCategory from './SubCategory'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <SubCategory/>
     </div>
   )
 }
---
+++ client/src/pages/home/SubCategory.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const SubCategory = () => {
+  return (
+    <div>SubCategory</div>
+  )
+}
+
+export default SubCategory
